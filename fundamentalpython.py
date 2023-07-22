b19b808e82b10a117fa710209642cbae9efa68f3
Ayush Arya <2020pgicsayush13@poornima.org>
2023-07-22 10:17:45 +0530

    git setup

--- fundamentalpython.py
+++ fundamentalpython.py
@@ -1,2 +1,7 @@
 # remote repository connect
   #git remote add origin"https://github.com/Ayush888-coder/piet_AI_7_23.git"
+#Daily process 
+#save and commit all changes
+ #git add.
+ #git commit -m "First commit"
+#push the changes 
