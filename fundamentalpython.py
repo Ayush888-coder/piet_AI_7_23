f34a43a5d7dc16a273479821449ef8c88ba834b4
Ayush Arya <2020pgicsayush13@poornima.org>
2023-07-24 10:39:32 +0530

    data types

--- fundamentalpython.py
+++ fundamentalpython.py
@@ -1,3 +1,43 @@
+#Brackets 
+() -Arg, Expressions,tuple
+{} -dictionary, set
+[] -indexing/subscripting/slicing, list
+<> -NameError
+#Identifiers
+ -It must contain alph-numeric only
+ -special character not allowed except underscore
+ -Cna not start from numbers
+#Operators
+1. Arithmetic :+,-,*,/,%(modulo), **(power), //(floor div)
+2. Assignment: =, +=, -=, .........
+3. Relational : >, <, >=, <=, ==, !=
+4. Logical: and, or, not
+5. Bitwise : & | ^, 
+6. identity operator
+#Data types
+1 int           10
+2 float         10.0
+3 complex       10+0j
+4 str            'hi' "hi"  '''hi'''
+      imutable
+5 list          [5,9,'t.9/3']
+unstructured, mutable collection of the DeprecationWarning
+6 tuple 
+7 dictionary
+8 set {5,7,54,3,'hi'}
+unordered collection of unique items 
+9 bool        true,False
+10 NoneType   None
+
+#Concept of mutability
+  Works on collections only
+  i. Item Access
+      -Indexing
+      -Sub-scripting
+      -Slicing
+  ii. Item Insertion
+  iii. Item Assignment
+ iv. Item deletion
 # remote repository connect
   #git remote add origin"https://github.com/Ayush888-coder/piet_AI_7_23.git"
 #Daily process 
